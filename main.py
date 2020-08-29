5ead4ae5d52c0209ab6c2fcf93a4b5ade1271b63
XinyiY5166 <xvy5166@psu.edu>
2020-08-29 17:23:27 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,3 @@
+celcius = float(input("Enter temperature in celcius:"))
+Fahrenheit = (celcius - 32) *(5/9)
+print(f"{celcius} \N{degree sign} in Celcius is equivalent to {Fahrenheit} \N{degree sign} Fahrenheit")
